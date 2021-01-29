9417fb39d12bfa18717eb1df292fa3f8834e37d9
shimaa hamdy <shimaahmady722@gmail.com>
2021-01-29 21:02:08 +0200

    implement check premission, verifiy_decode jwt

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -1,5 +1,5 @@
 import json
-from flask import request, _request_ctx_stack
+from flask import request, _request_ctx_stack, jsonify, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
@@ -37,7 +37,7 @@ def get_token_auth_header():
 
     #raise an AuthError if no header is present
     if not auth_header:
-        return header_missing
+        raise header_missing
 
     #split bearer and the token
     header_parts = auth_header.split()
@@ -48,22 +48,17 @@ def get_token_auth_header():
     
     #check if header in barrer_token form
     if len(header_parts)>2 or not barrer:
-        return barrer_token
+        raise malformed_auth
     #check first term is bearer
     if barrer.lower() != 'bearer':
-        return barrer_not_found
+        raise barrer_not_found
     #check if token is send
     if not token:
-        return token_not_found
+        raise token_not_found
 
     #return token
     return token
 
-    
-
-
-
-   raise Exception('Not Implemented')
 
 '''
 @TODO implement check_permissions(permission, payload) method
@@ -77,8 +72,12 @@ def get_token_auth_header():
     return true otherwise
 '''
 def check_permissions(permission, payload):
-    raise Exception('Not Implemented')
-
+    #raise an AuthError if permissions are not included in the payload
+    if 'permissions' not in payload:
+        abort(400)
+    #raise an AuthError if the requested permission string is not in the payload permissions array
+    if permission not in payload['permissions']:
+        abort(403)
 '''
 @TODO implement verify_decode_jwt(token) method
     @INPUTS
@@ -93,7 +92,49 @@ def check_permissions(permission, payload):
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
 def verify_decode_jwt(token):
-    raise Exception('Not Implemented')
+    #get jwks from Auth0
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    #an Auth0 token with key id (kid)
+    if 'kid' not in unverified_header:
+        raise malformed_auth
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    #verify the token using Auth0 /.well-known/jwks.json
+    if rsa_key:
+        #decode the payload from the token
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+
+            return payload
+
+        #validate the claims
+        except jwt.ExpiredSignatureError:
+            raise token_expired
+
+        except jwt.JWTClaimsError:
+            raise incorrect_clamis
+        
+        except Exception:
+            raise not_pars_auth
+    
+    raise not_found_key
 
 '''
 @TODO implement @requires_auth(permission) decorator method
@@ -129,12 +170,47 @@ barrer_not_found = AuthError({
     'description': 'Authorization header must start with "Bearer".'
     }, 401)
 
-barrer_token = AuthError({
+malformed_auth = AuthError({
     'code': 'invalid_header',
-    'description': 'Authorization header must be in form Bearer token.'
+    'description': 'Authorization malformed.'
     }, 401)
 
 token_not_found = AuthError({
     'code': 'invalid_header',
     'description': 'Token not found.'
     }, 401)
+
+token_expired = AuthError({
+    'code': 'token_expired',
+    'description': 'Token expired.'
+    }, 401)
+
+incorrect_clamis = AuthError({
+    'code': 'invalid_claims',
+    'description': 'Incorrect claims. check the audience and issuer.'
+    }, 401)
+
+not_pars_auth = AuthError({
+    'code': 'invalid_header',
+    'description': 'Unable to parse authentication token.'
+    }, 400)
+
+not_found_key = AuthError({
+    'code': 'invalid_header',
+    'description': 'Can not find the appropriate key.'
+    }, 400)
+#@app.errorhandler(400)
+#def bad_request(error):
+#    return jsonify({
+#        "success": False, 
+#        "error": 400,
+#        "message": "bad request structure"
+#      }), 400
+
+#@app.errorhandler(403)
+#def bad_request(error):
+#    return jsonify({
+#        "success": False, 
+#        "error": 403,
+#        "message": "forbidden requiest:Permission Not found"
+#      }), 403
