f1f044c69af8c21d1b46f0428d4a60704c081dc8
shimaa hamdy <shimaahmady722@gmail.com>
2021-01-31 03:29:00 +0200

    setup fronend

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'http://localhost:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'idandauth.us', // the auth0 domain prefix
+    audience: 'http://localhost:5000', // the audience set for the auth0 app
+    clientId: 'h96ZTbtfAeuPbEpwpIqceflQR853dEEp', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
